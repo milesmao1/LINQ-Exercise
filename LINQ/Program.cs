c4c5d74a285df06e0e923d02942d46b79798b290
Miles Mao <mrmao1@gmail.com>
2023-01-28 12:41:07 -0600

    LINQ-Exercise results

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var tvShowsList = new List<string>() { "Suits","Jack Ryan", "Elementary","Ozark", "The Blacklist"};
+
+            //var tvShowsListOrdered = tvShowsList.OrderBy(name => name.Length).ToList();
+
+            //foreach (var tvShow in tvShowsListOrdered) 
+            //{ 
+            //    Console.WriteLine(tvShow);
+            //}
+
+           // tvShowsListOrdered.ForEach(Console.WriteLine);
+
+            tvShowsList.OrderBy(name => name.Length).ToList().ForEach(Console.WriteLine);
         }
     }
 }
